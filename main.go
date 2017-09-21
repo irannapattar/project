f50b88996b856c5f5dd2159a9e55386edf9992dd
Irappa Pattar <irappapattar30@gmail.com>
2017-09-21 15:22:43 +0530

    updated main.go file with latest code

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
-	"project/hello"
+	//"project/hello"
+	"fmt"
 )
 
 func main() {
-	hello.Helloworld()
+	//hello.Helloworld()
+	total := hello()
+	fmt.Println("Total", total)
+}
+func hello() int {
+	total := 0
+	for i := 0; i < 5; i++ {
+		total += i
+	}
+	return total
 }
